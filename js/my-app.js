8000af8d510be70739715db8269774778269693a
Kenny Lim <myself@akita-ken.net>
2015-10-10 21:00:25 +0800

    Pushing changes for attempted leaflet.js kennel map

--- js/my-app.js
+++ js/my-app.js
@@ -18,3 +18,41 @@ var mainView = myApp.addView('.view-main', {
 myApp.onPageInit('dog', function(page) {
     // code here gets run when 'dog' page (<div data-page="dog">) is loaded
 });
+
+myApp.onPageInit('kennelmap', function(page) {
+  //var southWest = L.latLng(0, 0),
+    //northEast = L.latLng(800, 500),
+   // bounds = L.latLngBounds(southWest, northEast);
+
+  var map = L.map('map', {
+    center: [-350, 370],
+    zoom: 1,
+    touchZoom: false,
+    scrollWheelZoom: false,
+    doubleClickZoom: false,
+    boxZoom: false,
+    zoomControl: false,
+    attributionControl: false,
+    keyboard: false,
+    unloadInvisibleTiles: true,
+    reuseTiles: true,
+    crs: L.CRS.Simple
+  });
+
+  L.tileLayer('img/kennel-map.png', {
+    tileSize: 1331, 
+    noWrap: true, 
+    continuousWorld: true,
+    minZoom: 1,
+    maxZoom: 1,
+    //bounds: bounds
+  }).addTo(map);
+
+  var ccd = L.polygon([
+    [0, 0],
+    [0, 0],
+    [10, 10],
+    [10, 50]
+]).addTo(map);
+
+});
