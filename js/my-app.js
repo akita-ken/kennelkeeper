bb5fbe6b510acc87df790d8a2a382ea084d78125
Chiang Fong Lee <myself@cflee.net>
2015-10-10 22:29:46 +0800

    do some weird fixes to map and polygons

--- js/my-app.js
+++ js/my-app.js
@@ -25,13 +25,15 @@ myApp.onPageInit('kennelmap', function(page) {
    // bounds = L.latLngBounds(southWest, northEast);
 
   var map = L.map('map', {
-    center: [-350, 370],
+    center: [0, 0],
     zoom: 1,
-    touchZoom: false,
+    minZoom: 1,
+    maxZoom: 2,
+    /*touchZoom: false,
     scrollWheelZoom: false,
     doubleClickZoom: false,
     boxZoom: false,
-    zoomControl: false,
+    zoomControl: false,*/
     attributionControl: false,
     keyboard: false,
     unloadInvisibleTiles: true,
@@ -39,20 +41,52 @@ myApp.onPageInit('kennelmap', function(page) {
     crs: L.CRS.Simple
   });
 
+  var w = 2331,
+      h = 2331,
+      url = 'img/kennel-map.png';
+
+  // unproject() takes in a Point (and zoom level), and returns a LatLng
+  // "unproject" the image x/y coordinates to map lat/long coordinates
+  var southWest = map.unproject([0, h], 3);
+  var northEast = map.unproject([w, 0], 3);
+  var bounds = new L.LatLngBounds(southWest, northEast);
+  L.imageOverlay(url, bounds).addTo(map);
+  map.setMaxBounds(bounds);
+  /*
   L.tileLayer('img/kennel-map.png', {
-    tileSize: 1331, 
+    tileSize: 500, 
     noWrap: true, 
     continuousWorld: true,
     minZoom: 1,
     maxZoom: 1,
     //bounds: bounds
   }).addTo(map);
-
+  */
+  /*
   var ccd = L.polygon([
     [0, 0],
-    [0, 0],
+    [10, 0],
     [10, 10],
-    [10, 50]
-]).addTo(map);
+    [0, 10]
+  ]).addTo(map);
+  */
+
+  
+  var pingpong = L.polygon(computeBounds(-138, -159, 0, 35)).addTo(map);
+  var ccd = L.polygon(computeBounds(-159, -180, 0, 35)).addTo(map);
+  var lucky = L.polygon(computeBounds(-180, -200, 0, 35)).addTo(map);
+
+  /*
+      +x
+       ^
+       |
+       |
+       +-----> +y
+  */
+  // figures out the bounds for a rectangle from x1 to x2
+  // and from y1 to y2
+  function computeBounds(x1, x2, y1, y2) {
+    return [[x1, y1], [x2, y1], [x2, y2], [x1,y2]];
+  }
 
 });
