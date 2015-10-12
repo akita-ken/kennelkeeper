75e915ef602e12f60fb495fd1535c6f1b49be985
Kenny Lim <myself@akita-ken.net>
2015-10-12 11:18:36 +0800

    Adds KO observable to dog walk state. Status now updates when button is clicked, however walk state button text toggle logic still does not work. Removes implementation in the meantime.

--- js/dogs.js
+++ js/dogs.js
@@ -4,8 +4,8 @@ var dogs = ko.observableArray([
       name: "Candy",
       gender: 0,
       kennel: 14,
-      showered: "2015-08-02T18:25:43.511Z",
-      walked: true,
+      showered: new Date("2015-08-02T18:25:43"),
+      walked: ko.observable("Yes"),
       behaviour: "",
       medical: "",
       incident: [
@@ -16,19 +16,19 @@ var dogs = ko.observableArray([
       name: "Coco",
       gender: 0,
       kennel: 4,
-      showered: "2015-08-02T18:25:43.511Z",
-      walked: false,
+      showered: new Date("2015-08-02T18:25:43"),
+      walked: ko.observable("No"),
       behaviour: "Aggressive towards cyclists. Attacks other dogs.",
       medical: "Limb injury. If she starts limping again, please feed her with 25mg of metacam, that can be found in the medical box labelled 1A in the kitchen.",
       incident: [
         {
           log: "Attacked a passer-by, who got scratched quite badly. Be careful!",
-          date: "2015-06-18T13:50:43.511Z",
+          date: new Date("2015-06-18T13:50:43"),
           submitter: "Kristine"
         },
         {
           log: "Suddenly jumped out at a passing cyclist, causing him to swerve and nearly fall! Short leash and keep her away from cyclists.",
-          date: "2015-05-07T14:47:43.511Z",
+          date: new Date("2015-05-07T14:47:43"),
           submitter: "Alex"
         }
       ]
@@ -37,8 +37,8 @@ var dogs = ko.observableArray([
       name: "Dee Dee", 
       gender: 0,
       kennel: 18,
-      showered: "2015-08-02T18:25:43.511Z",
-      walked: true,
+      showered: new Date("2015-08-02T18:25:43"),
+      walked: ko.observable("Yes"),
       behaviour: "",
       medical: "",
       incident: [
@@ -49,8 +49,8 @@ var dogs = ko.observableArray([
       name: "Lego", 
       gender: 1,
       kennel: 20,
-      showered: "2015-08-02T18:25:43.511Z",
-      walked: true,
+      showered: new Date("2015-08-02T18:25:43"),
+      walked: ko.observable("Yes"),
       behaviour: "",
       medical: "",
       incident: [
@@ -61,8 +61,8 @@ var dogs = ko.observableArray([
       name: "Ginger", 
       gender: 0,
       kennel: 12,
-      showered: "2015-08-02T18:25:43.511Z",
-      walked: true,
+      showered: new Date("2015-08-02T18:25:43"),
+      walked: ko.observable("Yes"),
       behaviour: "",
       medical: "",
       incident: [
@@ -73,8 +73,8 @@ var dogs = ko.observableArray([
       name: "Lucky", 
       gender: 1,
       kennel: 14,
-      showered: "2015-08-02T18:25:43.511Z",
-      walked: true,
+      showered: new Date("2015-08-02T18:25:43"),
+      walked: ko.observable("Yes"),
       behaviour: "",
       medical: "",
       incident: [
@@ -85,8 +85,8 @@ var dogs = ko.observableArray([
       name: "Mentos", 
       gender: 1,
       kennel: 16,
-      showered: "2015-08-02T18:25:43.511Z",
-      walked: true,
+      showered: new Date("2015-08-02T18:25:43"),
+      walked: ko.observable("Yes"),
       behaviour: "",
       medical: "",
       incident: [
@@ -97,8 +97,8 @@ var dogs = ko.observableArray([
       name: "Charlie", 
       gender: 1,
       kennel: 2,
-      showered: "2015-08-02T18:25:43.511Z",
-      walked: true,
+      showered: new Date("2015-08-02T18:25:43"),
+      walked: ko.observable("Yes"),
       behaviour: "",
       medical: "",
       incident: [
@@ -136,17 +136,21 @@ function AppViewModel() {
 
   self.retrieveDog = function(dog) {
     self.activeDog = dog;
+    self.activeDogWalkStatus = ko.observable(self.activeDog.walked);
     return true;
   };
 
   self.walkDog = function() {
-    self.activeDog.walked = "walking";
+    self.activeDog.walked("Walking");
+    self.walking.push(
+      {
+        name: self.activeDog.name,
+        started: Date.now()
+      }
+    )
   };
 
   self.walkStatus = ko.computed(function() {
-    if(self.activeDog != null && self.activeDog.walked == "walking") {
-      return "End walk";
-    }
-    return "Walk";
+    
   });
 }
