01dce134b6d1b3551d4e032ab8e6791aae9476d2
Chiang Fong Lee <myself@cflee.net>
2015-10-13 19:21:44 +0800

    fix wrong 'create-dig' page name

--- js/my-app.js
+++ js/my-app.js
@@ -24,7 +24,7 @@ function applyKoBindings(page) {
   ko.applyBindings(appViewModel, pageElement);
 }
 
-var pages = ['dog', 'all-dogs', 'create-dig', 'edit-dog', 'showerme', 'walkme', 'walkingnow'];
+var pages = ['dog', 'all-dogs', 'create-dog', 'edit-dog', 'showerme', 'walkme', 'walkingnow'];
 pages.forEach(function(page) {
   myApp.onPageInit(page, applyKoBindings);
 });
