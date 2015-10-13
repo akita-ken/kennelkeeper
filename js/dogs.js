2a9b9bd2c97adf79c12d4c54d3423f3fd894c897
Chiang Fong Lee <myself@cflee.net>
2015-10-13 14:46:40 +0800

    dogs.js style changes

--- js/dogs.js
+++ js/dogs.js
@@ -1,118 +1,118 @@
 // This observable array contains hardcoded dogs
 var dogs = ko.observableArray([
-    { 
-      name: "Candy",
-      gender: 0,
-      kennel: 14,
-      showered: ko.observable(new Date("2015-08-02T18:25:43")),
-      walked: ko.observable("Yes"),
-      lastWalked: ko.observable(new Date("2015-08-02T18:25:43")),
-      behaviour: "",
-      medical: "",
-      incident: [
-        
-      ]
-    },
-    { 
-      name: "Coco",
-      gender: 0,
-      kennel: 4,
-      showered: ko.observable(new Date("2015-08-02T18:25:43")),
-      walked: ko.observable("No"),
-      lastWalked: ko.observable(new Date("2015-08-02T18:25:43")),
-      behaviour: "Aggressive towards cyclists. Attacks other dogs.",
-      medical: "Limb injury. If she starts limping again, please feed her with 25mg of metacam, that can be found in the medical box labelled 1A in the kitchen.",
-      incident: [
-        {
-          log: "Attacked a passer-by, who got scratched quite badly. Be careful!",
-          date: new Date("2015-06-18T13:50:43"),
-          submitter: "Kristine"
-        },
-        {
-          log: "Suddenly jumped out at a passing cyclist, causing him to swerve and nearly fall! Short leash and keep her away from cyclists.",
-          date: new Date("2015-05-07T14:47:43"),
-          submitter: "Alex"
-        }
-      ]
-    },
-    { 
-      name: "Donut", 
-      gender: 0,
-      kennel: 18,
-      showered: ko.observable(new Date("2015-08-02T18:25:43")),
-      walked: ko.observable("Yes"),
-      lastWalked: ko.observable(new Date("2015-08-02T18:25:43")),
-      behaviour: "",
-      medical: "",
-      incident: [
-        
-      ]
-    },
-    { 
-      name: "Lego", 
-      gender: 1,
-      kennel: 20,
-      showered: ko.observable(new Date("2015-08-02T18:25:43")),
-      walked: ko.observable("Yes"),
-      lastWalked: ko.observable(new Date("2015-08-02T18:25:43")),
-      behaviour: "",
-      medical: "",
-      incident: [
-        
-      ]
-    },
-    { 
-      name: "Ginger", 
-      gender: 0,
-      kennel: 12,
-      showered: ko.observable(new Date("2015-08-02T18:25:43")),
-      walked: ko.observable("Yes"),
-      lastWalked: ko.observable(new Date("2015-08-02T18:25:43")),
-      behaviour: "",
-      medical: "",
-      incident: [
-        
-      ]
-    },
-    { 
-      name: "Lucky", 
-      gender: 1,
-      kennel: 14,
-      showered: ko.observable(new Date("2015-08-02T18:25:43")),
-      walked: ko.observable("Yes"),
-      lastWalked: ko.observable(new Date("2015-08-02T18:25:43")),
-      behaviour: "",
-      medical: "",
-      incident: [
-        
-      ]
-    },
-    { 
-      name: "Mentos", 
-      gender: 1,
-      kennel: 16,
-      showered: ko.observable(new Date("2015-08-02T18:25:43")),
-      walked: ko.observable("Yes"),
-      lastWalked: ko.observable(new Date("2015-08-02T18:25:43")),
-      behaviour: "",
-      medical: "",
-      incident: [
-        
-      ] 
-    },
-    { 
-      name: "Charlie", 
-      gender: 1,
-      kennel: 2,
-      showered: ko.observable(new Date("2015-08-02T18:25:43")),
-      walked: ko.observable("Yes"),
-      lastWalked: ko.observable(new Date("2015-08-02T18:25:43")),
-      behaviour: "",
-      medical: "",
-      incident: [
-        
-      ]
-    }
+  { 
+    name: "Candy",
+    gender: 0,
+    kennel: 14,
+    showered: ko.observable(new Date("2015-08-02T18:25:43")),
+    walked: ko.observable("Yes"),
+    lastWalked: ko.observable(new Date("2015-08-02T18:25:43")),
+    behaviour: "",
+    medical: "",
+    incident: [
+      
+    ]
+  },
+  { 
+    name: "Coco",
+    gender: 0,
+    kennel: 4,
+    showered: ko.observable(new Date("2015-08-02T18:25:43")),
+    walked: ko.observable("No"),
+    lastWalked: ko.observable(new Date("2015-08-02T18:25:43")),
+    behaviour: "Aggressive towards cyclists. Attacks other dogs.",
+    medical: "Limb injury. If she starts limping again, please feed her with 25mg of metacam, that can be found in the medical box labelled 1A in the kitchen.",
+    incident: [
+      {
+        log: "Attacked a passer-by, who got scratched quite badly. Be careful!",
+        date: new Date("2015-06-18T13:50:43"),
+        submitter: "Kristine"
+      },
+      {
+        log: "Suddenly jumped out at a passing cyclist, causing him to swerve and nearly fall! Short leash and keep her away from cyclists.",
+        date: new Date("2015-05-07T14:47:43"),
+        submitter: "Alex"
+      }
+    ]
+  },
+  { 
+    name: "Donut", 
+    gender: 0,
+    kennel: 18,
+    showered: ko.observable(new Date("2015-08-02T18:25:43")),
+    walked: ko.observable("Yes"),
+    lastWalked: ko.observable(new Date("2015-08-02T18:25:43")),
+    behaviour: "",
+    medical: "",
+    incident: [
+      
+    ]
+  },
+  { 
+    name: "Lego", 
+    gender: 1,
+    kennel: 20,
+    showered: ko.observable(new Date("2015-08-02T18:25:43")),
+    walked: ko.observable("Yes"),
+    lastWalked: ko.observable(new Date("2015-08-02T18:25:43")),
+    behaviour: "",
+    medical: "",
+    incident: [
+      
+    ]
+  },
+  { 
+    name: "Ginger", 
+    gender: 0,
+    kennel: 12,
+    showered: ko.observable(new Date("2015-08-02T18:25:43")),
+    walked: ko.observable("Yes"),
+    lastWalked: ko.observable(new Date("2015-08-02T18:25:43")),
+    behaviour: "",
+    medical: "",
+    incident: [
+      
+    ]
+  },
+  { 
+    name: "Lucky", 
+    gender: 1,
+    kennel: 14,
+    showered: ko.observable(new Date("2015-08-02T18:25:43")),
+    walked: ko.observable("Yes"),
+    lastWalked: ko.observable(new Date("2015-08-02T18:25:43")),
+    behaviour: "",
+    medical: "",
+    incident: [
+      
+    ]
+  },
+  { 
+    name: "Mentos", 
+    gender: 1,
+    kennel: 16,
+    showered: ko.observable(new Date("2015-08-02T18:25:43")),
+    walked: ko.observable("Yes"),
+    lastWalked: ko.observable(new Date("2015-08-02T18:25:43")),
+    behaviour: "",
+    medical: "",
+    incident: [
+      
+    ] 
+  },
+  { 
+    name: "Charlie", 
+    gender: 1,
+    kennel: 2,
+    showered: ko.observable(new Date("2015-08-02T18:25:43")),
+    walked: ko.observable("Yes"),
+    lastWalked: ko.observable(new Date("2015-08-02T18:25:43")),
+    behaviour: "",
+    medical: "",
+    incident: [
+      
+    ]
+  }
 ]);
 
 function AppViewModel() {
@@ -139,15 +139,15 @@ function AppViewModel() {
 
   self.addDog = function() {
     var newDog = {
-        name: self.createDogName,
-        gender: self.createDogGender,
-        kennel: 0, // this needs to be changed to a function
-        showered: ko.observable(null),
-        walked: ko.observable("No"),
-        behaviour: self.createDogBehaviour,
-        medical: self.createDogMedical,
-        incident: []
-      }
+      name: self.createDogName,
+      gender: self.createDogGender,
+      kennel: 0, // this needs to be changed to a function
+      showered: ko.observable(null),
+      walked: ko.observable("No"),
+      behaviour: self.createDogBehaviour,
+      medical: self.createDogMedical,
+      incident: []
+    }
     self.dog.push(newDog);
     self.activeDog = newDog;
     self.initVars();
@@ -157,7 +157,7 @@ function AppViewModel() {
 
   self.resetButton = function() {
     self.justAddedDog = false;
-  }
+  };
 
   self.removeDog = function() {
     self.dog.remove(this);
@@ -169,16 +169,14 @@ function AppViewModel() {
   };
 
   self.walkButton = function() {
-    if(self.activeDog.walked() == "No") {
+    if (self.activeDog.walked() == "No") {
       self.activeDog.walked("Walking");
-      self.walking.push(
-        {
-          name: self.activeDog.name,
-          started: Date.now(),
-          walker: "TestUser"
-        }
-      )
-    } else if(self.activeDog.walked() == "Walking") {
+      self.walking.push({
+        name: self.activeDog.name,
+        started: Date.now(),
+        walker: "TestUser"
+      })
+    } else if (self.activeDog.walked() == "Walking") {
       alert("Dog is happy, let's go! :3");
       pos = self.walking.map(function(e) {
         return e.name; 
@@ -190,20 +188,18 @@ function AppViewModel() {
     }
   };
 
-  self.walkStatus = ko.computed(
-    {
-      read: function() {
-        if(self.activeDog.walked() == "No") {
-          return "Walk";
-        } else if(self.activeDog.walked() == "Walking") {
-          return "End walk";
-        } else {
-          return "Already walked";
-        }
-      },
-      deferEvaluation: true
-    }
-  );
+  self.walkStatus = ko.computed({
+    read: function() {
+      if (self.activeDog.walked() == "No") {
+        return "Walk";
+      } else if (self.activeDog.walked() == "Walking") {
+        return "End walk";
+      } else {
+        return "Already walked";
+      }
+    },
+    deferEvaluation: true
+  });
 
   self.showerDog = function() {
     console.log("hey");
@@ -216,7 +212,7 @@ function AppViewModel() {
   }
 
   self.walkBadge = function(dog) {
-    if(dog.walked() == "No") {
+    if (dog.walked() == "No") {
       return true;
     }
     return false;
