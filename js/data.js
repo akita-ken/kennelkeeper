c939c36c403864a3dbd799199f49b505e826a6db
Chiang Fong Lee <myself@cflee.net>
2015-10-16 11:47:06 +0800

    Fix genders
    - Hard-coded data in data.js change to strings instead of 0/1
    - Fix <select> binding to be value instead of selectedOptions

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@
 var dogs = ko.observableArray([
   { 
     name: "Candy",
-    gender: 0,
+    gender: "Female",
     kennel: 22,
     showered: ko.observable(moment().subtract(11, 'days')),
     walked: ko.observable("Yes"),
@@ -18,7 +18,7 @@ var dogs = ko.observableArray([
   },
   { 
     name: "Coco",
-    gender: 0,
+    gender: "Female",
     kennel: 4,
     showered: ko.observable(moment().subtract(22, 'days')),
     walked: ko.observable("No"),
@@ -44,12 +44,12 @@ var dogs = ko.observableArray([
   },
   { 
     name: "Donut", 
-    gender: 0,
+    gender: "Female",
     kennel: 3,
     showered: ko.observable(moment().subtract(13, 'days')),
     walked: ko.observable("Yes"),
     lastWalked: ko.observable(moment().subtract(1, 'days').hours(10).minutes(42).seconds(30)),
-    behaviour: "",
+    behaviour: ko.observable(""),
     medical: "",
     photos: [
       "img/dogs/Donut/0.png"
@@ -60,12 +60,12 @@ var dogs = ko.observableArray([
   },
   { 
     name: "Lego", 
-    gender: 1,
+    gender: "Male",
     kennel: 21,
     showered: ko.observable(moment().subtract(13, 'days')),
     walked: ko.observable("No"),
     lastWalked: ko.observable(moment().subtract(1, 'days').hours(11).minutes(7).seconds(44)),
-    behaviour: "Lego is a shy dog, he may be startled by large vehicles. Walkers should be firm with him and give him confidence to continue walking. He has a low risk of biting.",
+    behaviour: ko.observable("Lego is a shy dog, he may be startled by large vehicles. Walkers should be firm with him and give him confidence to continue walking. He has a low risk of biting."),
     medical: "Used to have heartworm but has since recovered since January 2015.",
     photos: [
       "img/dogs/Lego/0.png"
@@ -76,12 +76,12 @@ var dogs = ko.observableArray([
   },
   { 
     name: "Ginger", 
-    gender: 0,
+    gender: "Female",
     kennel: 24,
     showered: ko.observable(moment().subtract(13, 'days')),
     walked: ko.observable("Yes"),
     lastWalked: ko.observable(moment().subtract(1, 'days').hours(11).minutes(34).seconds(30)),
-    behaviour: "",
+    behaviour: ko.observable(""),
     medical: "",
     photos: [
       "img/dogs/Ginger/0.png"
@@ -92,12 +92,12 @@ var dogs = ko.observableArray([
   },
   { 
     name: "Lucky", 
-    gender: 1,
+    gender: "Male",
     kennel: 2,
     showered: ko.observable(moment().subtract(13, 'days')),
     walked: ko.observable("Walking"),
     lastWalked: ko.observable(moment().subtract(1, 'days').hours(13).minutes(15).seconds(0)),
-    behaviour: "A peaceful dog.",
+    behaviour: ko.observable("A peaceful dog."),
     medical: "",
     photos: [
       "img/dogs/Lucky/0.png"
@@ -108,12 +108,12 @@ var dogs = ko.observableArray([
   },
   { 
     name: "Mentos", 
-    gender: 1,
+    gender: "Male",
     kennel: 8,
     showered: ko.observable(moment().subtract(34, 'days')),
     walked: ko.observable("No"),
     lastWalked: ko.observable(moment().subtract(1, 'days').hours(9).minutes(12).seconds(30)),
-    behaviour: "Mentos is an active and playful dog, do keep him safe when walking near large drains.",
+    behaviour: ko.observable("Mentos is an active and playful dog, do keep him safe when walking near large drains."),
     medical: "",
     photos: [
       "img/dogs/Mentos/0.png"
@@ -124,12 +124,12 @@ var dogs = ko.observableArray([
   },
   { 
     name: "Charlie", 
-    gender: 1,
+    gender: "Male",
     kennel: 3,
     showered: ko.observable(moment().subtract(7, 'days')),
     walked: ko.observable("Yes"),
     lastWalked: ko.observable(moment().subtract(1, 'days').hours(11).minutes(54).seconds(30)),
-    behaviour: "",
+    behaviour: ko.observable(""),
     medical: "",
     photos: [
       "img/dogs/Charlie/0.png"
@@ -140,12 +140,12 @@ var dogs = ko.observableArray([
   },
   { 
     name: "Pringles", 
-    gender: 0,
+    gender: "Female",
     kennel: 11,
     showered: ko.observable(moment().subtract(35, 'days')),
     walked: ko.observable("Yes"),
     lastWalked: ko.observable(moment().subtract(1, 'days').hours(12).minutes(05).seconds(30)),
-    behaviour: "Pringles responds very well to physical touch, walkers are encouraged to play with her so that she can be comfortable with humans.",
+    behaviour: ko.observable("Pringles responds very well to physical touch, walkers are encouraged to play with her so that she can be comfortable with humans."),
     medical: "",
     photos: [
       "img/dogs/Pringles/0.png"
@@ -156,12 +156,12 @@ var dogs = ko.observableArray([
   },
   { 
     name: "Ris", 
-    gender: 1,
+    gender: "Male",
     kennel: 12,
     showered: ko.observable(moment().subtract(13, 'days')),
     walked: ko.observable("No"),
     lastWalked: ko.observable(moment().subtract(1, 'days').hours(12).minutes(27).seconds(30)),
-    behaviour: "",
+    behaviour: ko.observable(""),
     medical: "",
     photos: [
       "img/dogs/Ris/0.png"
@@ -172,12 +172,12 @@ var dogs = ko.observableArray([
   },
   { 
     name: "Kyoto", 
-    gender: 1,
+    gender: "Male",
     kennel: 14,
     showered: ko.observable(moment().subtract(13, 'days')),
     walked: ko.observable("Yes"),
     lastWalked: ko.observable(moment().subtract(1, 'days').hours(12).minutes(41).seconds(30)),
-    behaviour: "",
+    behaviour: ko.observable(""),
     medical: "",
     photos: [
       "img/dogs/Kyoto/0.png"
@@ -188,12 +188,12 @@ var dogs = ko.observableArray([
   },
   { 
     name: "Muthu", 
-    gender: 1,
+    gender: "Male",
     kennel: 15,
     showered: ko.observable(moment().subtract(13, 'days')),
     walked: ko.observable("Yes"),
     lastWalked: ko.observable(moment().subtract(1, 'days').hours(12).minutes(55).seconds(30)),
-    behaviour: "",
+    behaviour: ko.observable(""),
     medical: "",
     photos: [
       "img/dogs/Muthu/0.png"
@@ -204,12 +204,12 @@ var dogs = ko.observableArray([
   },
   { 
     name: "Pappy", 
-    gender: 1,
+    gender: "Male",
     kennel: 14,
     showered: ko.observable(moment().subtract(13, 'days')),
     walked: ko.observable("Yes"),
     lastWalked: ko.observable(moment().subtract(1, 'days').hours(13).minutes(20).seconds(30)),
-    behaviour: "",
+    behaviour: ko.observable(""),
     medical: "",
     photos: [
       "img/dogs/Pappy/0.png"
